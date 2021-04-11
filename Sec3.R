dd6f57f0daca26af8f711a547e8b7dcb1e5faec0
wangbt <wangbertone@hotmail.com>
2021-04-10 21:33:38 -0500

    Finished sec 3

--- Sec3.R
+++ Sec3.R
@@ -413,14 +413,324 @@ tab <- data.frame(x = s)
 tab %>% separate(x, c("feet","inches"), sep = "'", fill = "right")
 tab %>% extract(x, c("feet", "inches"), regex = "(\\d)'(\\d{1,2})")
 
-
+# case1 Many students measuring exactly 5 or 6 feet did not enter any inches. For example, 6'
+# - our pattern requires that inches be included.
 yes <- c("5", "6", "5")
 no <- c("5'", "5''", "5'4")
 s <- c(yes, no)
 str_replace(s, "^([4-7])$", "\\1'0")
 
+# case2 and 4
 str_replace(s, "^([56])'?$", "\\1'0")
 
 # str_detect(s, "^([56])'?$")
 
+# case 3
+# permit zero or one period . followed by zero or more digits. So we will use both ? and *.
+pattern <- "^[4-7]\\s*'\\s*(\\d+\\.?\\d*)$"
 
+# case 5 Some entries are in meters and some of these use European decimals: 1.6, 1,7.
+yes <- c("1,7", "1, 8", "2, " )
+no <- c("5,8", "5,3,2", "1.7")
+s <- c(yes, no)
+str_replace(s, "^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2")
+
+
+# Trimming
+s <- "Hi "
+cat(s)
+identical(s, "Hi")
+str_trim("5 ' 9 ")
+
+identical(str_trim(s), "Hi")
+
+# to upper and to lowecase.names
+s <- c("Five feet eight inches")
+str_to_lower(s)
+
+# Putting it into a function
+convert_format <- function(s){
+  s %>%
+    str_replace("feet|foot|ft", "'") %>% #convert feet symbols to '
+    str_replace_all("inches|in|''|\"|cm|and", "") %>%  #remove inches and other symbols
+    str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") %>% #change x.y, x,y x y
+    str_replace("^([56])'?$", "\\1'0") %>% #add 0 when to 5 or 6
+    str_replace("^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2") %>% #change european decimal
+    str_trim() #remove extra space
+}
+
+# We can also write a function that converts words to numbers:
+words_to_numbers <- function(s){
+  str_to_lower(s) %>%  
+    str_replace_all("zero", "0") %>%
+    str_replace_all("one", "1") %>%
+    str_replace_all("two", "2") %>%
+    str_replace_all("three", "3") %>%
+    str_replace_all("four", "4") %>%
+    str_replace_all("five", "5") %>%
+    str_replace_all("six", "6") %>%
+    str_replace_all("seven", "7") %>%
+    str_replace_all("eight", "8") %>%
+    str_replace_all("nine", "9") %>%
+    str_replace_all("ten", "10") %>%
+    str_replace_all("eleven", "11")
+}
+
+# Now we can see which problematic entries remain:
+converted <- problems %>% words_to_numbers %>% convert_format
+remaining_problems <- converted[not_inches_or_cm(converted)]
+pattern <- "^[4-7]\\s*'\\s*\\d+\\.?\\d*$"
+index <- str_detect(remaining_problems, pattern)
+remaining_problems[!index]
+
+
+# Putting it all together
+pattern <- "^([4-7])\\s*'\\s*(\\d+\\.?\\d*)$"
+
+smallest <- 50
+tallest <- 84
+new_heights <- reported_heights %>% 
+  mutate(original = height, 
+         height = words_to_numbers(height) %>% convert_format()) %>%
+  extract(height, c("feet", "inches"), regex = pattern, remove = FALSE) %>% 
+  mutate_at(c("height", "feet", "inches"), as.numeric) %>%
+  mutate(guess = 12*feet + inches) %>%
+  mutate(height = case_when(
+    !is.na(height) & between(height, smallest, tallest) ~ height, #inches 
+    !is.na(height) & between(height/2.54, smallest, tallest) ~ height/2.54, #centimeters
+    !is.na(height) & between(height*100/2.54, smallest, tallest) ~ height*100/2.54, #meters
+    !is.na(guess) & inches < 12 & between(guess, smallest, tallest) ~ guess, #feet'inches
+    TRUE ~ as.numeric(NA))) %>%
+  select(-guess)
+
+# We can check all the entries we converted using the following code:
+new_heights %>%
+  filter(not_inches(original)) %>%
+  select(original, height) %>% 
+  arrange(height) %>%
+  View()
+
+# Let's take a look at the shortest students in our dataset using the following code:
+new_heights %>% arrange(height) %>% head(n=7)
+
+
+
+# Sec 3.3 String processing Part 3####
+# read raw murders data line by line
+filename <- system.file("extdata/murders.csv", package = "dslabs")
+lines <- readLines(filename)
+lines %>% head()
+
+
+# split at commas with str_split function, remove row of column names
+x <- str_split(lines, ",") 
+x %>% head()
+col_names <- x[[1]]
+x <- x[-1]
+
+# extract first element of each list entry
+library(purrr)
+map(x, function(y) y[1]) %>% head()
+map(x, 1) %>% head()
+
+# extract columns 1-5 as characters, then convert to proper format - NOTE: DIFFERENT FROM VIDEO
+dat <- data.frame(parse_guess(map_chr(x, 1)),
+                  parse_guess(map_chr(x, 2)),
+                  parse_guess(map_chr(x, 3)),
+                  parse_guess(map_chr(x, 4)),
+                  parse_guess(map_chr(x, 5))) %>%
+  setNames(col_names)
+
+dat %>% head
+
+# more efficient code for the same thing
+dat <- x %>%
+  transpose() %>%
+  map( ~ parse_guess(unlist(.))) %>%
+  setNames(col_names) %>% 
+  as.data.frame() 
+
+# the simplify argument makes str_split return a matrix instead of a list
+x <- str_split(lines, ",", simplify = TRUE) 
+col_names <- x[1,]
+x <- x[-1,]
+x %>% as_data_frame() %>%
+  setNames(col_names) %>%
+  mutate_all(parse_guess)
+
+# case study: Extracting a Table from a PDF
+library(dslabs)
+data("research_funding_rates")
+research_funding_rates 
+
+library("pdftools")
+temp_file <- tempfile()
+url <- "http://www.pnas.org/content/suppl/2015/09/16/1510159112.DCSupplemental/pnas.201510159SI.pdf"
+download.file(url, temp_file)
+txt <- pdf_text(temp_file)
+file.remove(temp_file)
+
+raw_data_research_funding_rates <- txt[2]
+
+data("raw_data_research_funding_rates")
+
+raw_data_research_funding_rates %>% head
+
+tab <- str_split(raw_data_research_funding_rates, "\n")
+
+tab <- tab[[1]]
+
+tab %>% head
+
+the_names_1 <- tab[3]
+the_names_2 <- tab[4]
+
+the_names_1
+the_names_1 <- the_names_1 %>%
+  str_trim() %>%
+  str_replace_all(",\\s.", "") %>%
+  str_split("\\s{2,}", simplify = TRUE)
+the_names_1
+
+the_names_2
+the_names_2 <- the_names_2 %>%
+  str_trim() %>%
+  str_split("\\s+", simplify = TRUE)
+the_names_2
+
+tmp_names <- str_c(rep(the_names_1, each = 3), the_names_2[-1], sep = "_")
+the_names <- c(the_names_2[1], tmp_names) %>%
+  str_to_lower() %>%
+  str_replace_all("\\s", "_")
+the_names
+
+new_research_funding_rates <- tab[6:14] %>%
+  str_trim %>%
+  str_split("\\s{2,}", simplify = TRUE) %>%
+  data.frame(stringsAsFactors = FALSE) %>%
+  setNames(the_names) %>%
+  mutate_at(-1, parse_number)
+new_research_funding_rates %>% head()
+
+
+# We can see that the objects are identical:
+identical(research_funding_rates, new_research_funding_rates)
+
+#Recoding####
+# life expectancy time series for Caribbean countries
+library(dslabs)
+data("gapminder")
+gapminder %>% 
+  filter(region=="Caribbean") %>%
+  ggplot(aes(year, life_expectancy, color = country)) +
+  geom_line()
+
+# display long country names
+gapminder %>% 
+  filter(region=="Caribbean") %>%
+  filter(str_length(country) >= 12) %>%
+  distinct(country) 
+
+# recode long country names and remake plot
+gapminder %>% filter(region=="Caribbean") %>%
+  mutate(country = recode(country, 
+                          'Antigua and Barbuda'="Barbuda",
+                          'Dominican Republic' = "DR",
+                          'St. Vincent and the Grenadines' = "St. Vincent",
+                          'Trinidad and Tobago' = "Trinidad")) %>%
+  ggplot(aes(year, life_expectancy, color = country)) +
+  geom_line()
+
+# Assesment 3####
+schedule <- data.frame(day = c ('Monday','Tuesday'),
+                       staff = c("Mandy, Chris and Laura","Steve, Ruth and Frank") )
+
+schedule
+
+str_split(schedule$staff, ", | and ")
+str_split(schedule$staff, ",\\s|\\sand\\s")
+
+
+
+
+tidy <- schedule %>% 
+  mutate(staff = str_split(staff, ", | and ")) %>% 
+  unnest(cols = c('day','staff'))
+
+tidy <- schedule %>% 
+  mutate(staff = str_split(staff, ", | and ", simplify = TRUE)) %>% 
+  unnest(cols = c())
+
+tidy <- separate(schedule, staff, into = c("s1","s2","s3"), sep =" ,") %>% 
+  gather(key = s, value = staff, s1:s3)
+
+# Assesment part 2 ####
+# Import raw Brexit referendum polling data from Wikipedia:
+library(rvest)
+library(tidyverse)
+library(stringr)
+url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+tab <- read_html(url) %>% html_nodes("table")
+polls <- tab[[5]] %>% html_table(fill = TRUE)
+
+head(polls)
+
+colnames(polls) <- c("dates", "remain", "leave", "undecided", "lead", "samplesize", "pollster", "poll_type", "notes")
+
+nrow(polls %>% filter (str_detect(remain, "%")))
+
+#key#
+names(polls) <- c("dates", "remain", "leave", "undecided", "lead", "samplesize", "pollster", "poll_type", "notes")
+polls <- polls[str_detect(polls$remain, "%"), -9]
+nrow(polls)
+
+as.numeric(str_remove(polls$remain, "%"))
+as.numeric(polls$remain)/100
+parse_number(polls$remain)
+str_remove(polls$remain, "%")/100
+as.numeric(str_replace(polls$remain, "%", ""))/100
+parse_number(polls$remain)/100
+
+# function_name(polls$undecided, "arg1", "arg2").
+str_replace_all(polls$undecided,"N/A", "0")
+
+# The end date of the poll will always be one or two digits, 
+# followed by a space, followed by the month as one or more 
+# letters (either capital or lowercase). In these data, 
+# all month abbreviations or names have 3, 4 or 5 letters.
+
+a <- "\\d?\\s[a-zA-Z]?" # F
+b <- "\\d+\\s[a-zA-Z]+" # T
+c <- "\\d+\\s[A-Z]+"  # F
+d <- "[0-9]+\\s[a-zA-Z]+" #T
+e <- "\\d{1,2}\\s[a-zA-Z]+" #T
+f <- "\\d{1,2}[a-zA-Z]+"  # F
+g <- "\\d+\\s[a-zA-Z]{3,5}"  # T
+
+
+temp <- str_extract_all(polls$dates, a)
+end_date <- sapply(temp, function(x) x[length(x)]) # take last element (handles polls that cross month boundaries)
+
+
+temp <- str_extract_all(polls$dates, b)
+end_date <- sapply(temp, function(x) x[length(x)]) # take last element (handles polls that cross month boundaries)
+
+
+temp <- str_extract_all(polls$dates, c)
+end_date <- sapply(temp, function(x) x[length(x)]) # take last element (handles polls that cross month boundaries)
+
+
+temp <- str_extract_all(polls$dates, d)
+end_date <- sapply(temp, function(x) x[length(x)]) # take last element (handles polls that cross month boundaries)
+
+
+temp <- str_extract_all(polls$dates, e)
+end_date <- sapply(temp, function(x) x[length(x)]) # take last element (handles polls that cross month boundaries)
+
+
+temp <- str_extract_all(polls$dates, f)
+end_date <- sapply(temp, function(x) x[length(x)]) # take last element (handles polls that cross month boundaries)
+
+
+temp <- str_extract_all(polls$dates, g)
+end_date <- sapply(temp, function(x) x[length(x)]) # take last element (handles polls that cross month boundaries)
