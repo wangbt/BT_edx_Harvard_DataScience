d27c6842c64dab7ac36807c04b3545599fd1770d
wangbt <wangbertone@hotmail.com>
2021-04-14 00:41:08 -0500

    sec 1

--- LinearRegression/Sec1.R
+++ LinearRegression/Sec1.R
@@ -1,2 +1,11 @@
 #Data Science Linear Regression
 # Sec1 
+library(Lahman)
+library(tidyverse)
+library(dslabs)
+ds_theme_set()
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
+  ggplot(aes(HR_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
