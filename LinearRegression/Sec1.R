4f2f990489e64b354e82d18e1728907bc83d147a
wangbt <wangbertone@hotmail.com>
2021-04-15 22:08:27 -0500

    sec 1.3

--- LinearRegression/Sec1.R
+++ LinearRegression/Sec1.R
@@ -1,11 +1,149 @@
 #Data Science Linear Regression
-# Sec1 
+# sec1.1 ####
 library(Lahman)
 library(tidyverse)
 library(dslabs)
 ds_theme_set()
 
+# Code: Scatterplot of the relationship between HRs and wins
 Teams %>% filter(yearID %in% 1961:2001) %>%
   mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
   ggplot(aes(HR_per_game, R_per_game)) + 
   geom_point(alpha = 0.5)
+
+
+#Code: Scatterplot of the relationship between stolen bases and wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(SB_per_game = SB / G, R_per_game = R / G) %>%
+  ggplot(aes(SB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Code: Scatterplot of the relationship between bases on balls and runs
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_per_game = BB / G, R_per_game = R / G) %>%
+  ggplot(aes(BB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# Assessment 1.1 ####
+# You want to know whether teams with more at-bats per game have 
+# more runs per game.
+# A
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  ggplot(aes(AB, R)) + 
+  geom_point(alpha = 0.5)
+
+# B TRUE
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+# C
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_line()
+
+# D
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(R_per_game, AB_per_game)) + 
+  geom_point()
+
+# q6
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) +
+  geom_point(alpha = 0.5)
+
+# q7
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(W_per_game = W/G, E_per_game = E/G) %>%
+  ggplot(aes(E_per_game, W_per_game)) +
+  geom_point(alpha = 0.5)
+
+# q8
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(X2B_per_game = X2B/G, X3B_per_game = X3B/G) %>%
+  ggplot(aes(X2B_per_game, X3B_per_game)) +
+  geom_point(alpha = 0.5)
+
+# sec1.2####
+# create the dataset
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+set.seed(1983)
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+
+# means and standard deviations
+galton_heights %>%
+  summarize(mean(father), sd(father), mean(son), sd(son))
+
+# scatterplot of father and son heights
+galton_heights %>%
+  ggplot(aes(father, son)) +
+  geom_point(alpha = 0.5)
+
+galton_heights <- GaltonFamilies %>% 
+  filter(childNum == 1 & gender == "male") %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight) 
+
+galton_heights %>%
+  summarize((cor(father,son)))
+
+rho <- mean(scale(x)*scale(y))
+galton_heights %>% summarize(r = cor(father, son)) %>% pull(r)
+
+# Sample Correlation is a Random Variable
+# compute sample correlation
+R <- sample_n(galton_heights, 25, replace = TRUE) %>%
+  summarize(r = cor(father, son))
+R
+
+# Monte Carlo simulation to show distribution of sample correlation
+B <- 1000
+N <- 25
+R <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    summarize(r = cor(father, son)) %>%
+    pull(r)
+})
+length(R)
+
+qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+
+# expected value and standard error
+mean(R)
+sd(R)
+
+# QQ-plot to evaluate whether N is large enough
+data.frame(R) %>%
+  ggplot(aes(sample = R)) +
+  stat_qq() +
+  geom_abline(intercept = mean(R), slope = sqrt((1-mean(R)^2)/(N-2)))
+
+
+# assessment 1.2
+# q7 
+Teams %>% filter(yearID %in%  1961:2001) %>%
+  mutate(AB_per_game = AB/G, 
+         R_per_game = R/G,
+         W_per_game = W/G, 
+         E_per_game = E/G,
+         X2B_per_game = X2B/G, 
+         X3B_per_game = X3B/G) %>%
+  #summarize(r = cor(R_per_game,AB_per_game))
+  # summarize(r = cor(W_per_game,E_per_game))
+  summarize(r = cor(X2B_per_game, X3B_per_game))
+
+# sec 1.3####
+
+
