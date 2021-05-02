70546e275325d46c6afc7a1cf20db22e3e80a590
wangbt <wangbertone@hotmail.com>
2021-05-02 00:48:47 -0500

    sec 2.3 finished

--- LinearRegression/Sec2.R
+++ LinearRegression/Sec2.R
@@ -330,6 +330,265 @@ dat %>%
 
 # sec2.3 Tibbles, do, and broom####
 #Advanced dplyr: Tibbles
+# stratify by HR
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR = round(HR/G, 1), 
+         BB = BB/G,
+         R = R/G) %>%
+  select(HR, BB, R) %>%
+  filter(HR >= 0.4 & HR<=1.2)
+
+# calculate slope of regression lines to predict runs by BB in different HR strata
+dat %>%  
+  group_by(HR) %>%
+  summarize(slope = cor(BB,R)*sd(R)/sd(BB))
+
+# use lm to get estimated slopes - lm does not work with grouped tibbles
+dat %>%  
+  group_by(HR) %>%
+  lm(R ~ BB, data = .) %>%
+  .$coef
+
+# inspect a grouped tibble
+dat %>% group_by(HR) %>% head()
+dat %>% group_by(HR) %>% class()
+
+
+#Tibbles: Differences from Data Frames#
+# inspect data frame and tibble
+Teams
+as_tibble(Teams)
+# Note that the function was formerly called as.tibble()
+
+# subsetting a data frame sometimes generates vectors
+class(Teams[,20])
+
+# subsetting a tibble always generates tibbles
+class(as_tibble(Teams[,20]))
+
+# pulling a vector out of a tibble
+class(as_tibble(Teams)$HR)
+
+# access a non-existing column in a data frame or a tibble
+Teams$hr
+
+as_tibble(Teams)$HR
+class(as_tibble(Teams)$HR)
+as_tibble(Teams)$hr
+
+# create a tibble with complex objects
+tibble(id = c(1, 2, 3), func = c(mean, median, sd))
+
+
+# do#
+# use do to fit a regression line to each HR stratum
+dat %>%  
+  group_by(HR) %>%
+  do(fit = lm(R ~ BB, data = .))
+
+# using do without a column name gives an error
+dat %>%
+  group_by(HR) %>%
+  do(lm(R ~ BB, data = .))
+
+# define a function to extract slope from lm
+get_slope <- function(data){
+  fit <- lm(R ~ BB, data = data)
+  data.frame(slope = fit$coefficients[2], 
+             se = summary(fit)$coefficient[2,2])
+}
+
+# return the desired data frame
+dat %>%  
+  group_by(HR) %>%
+  do(get_slope(.))
+
+# not the desired output: a column containing data frames
+dat %>%  
+  group_by(HR) %>%
+  do(slope = get_slope(.))
+
+# dat %>%  
+#   group_by(HR) %>%
+#   mutate(new = get_slope(.))
+
+
+# data frames with multiple rows will be concatenated appropriately
+get_lse <- function(data){
+  fit <- lm(R ~ BB, data = data)
+  data.frame(term = names(fit$coefficients),
+             estimate = fit$coefficients, 
+             se = summary(fit)$coefficient[,2])
+}
+
+dat %>%  
+  group_by(HR) %>%
+  do(get_lse(.))
+
+
+# broom#
+# use tidy to return lm estimates and related information as a data frame
+library(broom)
+fit <- lm(R ~ BB, data = dat)
+tidy(fit)
+
+# add confidence intervals with tidy
+tidy(fit, conf.int = TRUE)
+
+# pipeline with lm, do, tidy
+dat %>%  
+  group_by(HR) %>%
+  do(tidy(lm(R ~ BB, data = .), conf.int = TRUE)) %>%
+  filter(term == "BB") %>%
+  select(HR, estimate, conf.low, conf.high)
+
+# dat %>%  
+#   group_by(HR) %>%
+#   do(tidy(lm(R ~ BB, data = .), conf.int = TRUE)) %>%
+#   filter(term == "(Intercept)") %>%
+#   select(HR, estimate, conf.low, conf.high)
+
+
+# make ggplots
+dat %>%  
+  group_by(HR) %>%
+  do(tidy(lm(R ~ BB, data = .), conf.int = TRUE)) %>%
+  filter(term == "BB") %>%
+  select(HR, estimate, conf.low, conf.high) %>%
+  ggplot(aes(HR, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  geom_errorbar() +
+  geom_point()
+
+# inspect with glance
+glance(fit)
+
+names(glance(fit))
+
+# assesment
+# q5 
+get_slope <- function(data) {
+  fit <- lm(R ~ BB, data = data)
+  sum.fit <- summary(fit)
+  
+  data.frame(slope = sum.fit$coefficients[2, "Estimate"], 
+             se = sum.fit$coefficients[2, "Std. Error"],
+             pvalue = sum.fit$coefficients[2, "Pr(>|t|)"])
+}
+
+# a
+dat %>% 
+  group_by(HR) %>% 
+  do(get_slope)
+
+# b TRUE  This will create a tibble with four columns: 
+# HR, slope, se, and pvalue for each level of HR.
+dat %>% 
+  group_by(HR) %>% # If you forget group_by(), then the results will be a model on the data as a whole, rather than on the data stratified by home runs.
+  do(get_slope(.))# The data frame must be passed to get_slope() using (.)
+# If you name the results of the do() command such as in the code do(slope = get_slope(.)), that will save all results in a single column called slope
+
+
+# c
+dat %>% 
+  group_by(HR) %>% 
+  do(slope = get_slope(.))
+
+# d
+dat %>% 
+  do(get_slope(.))
+
+
+# q7
+# You want to know whether the relationship between home runs and runs per
+# game varies by baseball league. You create the following dataset:
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR = HR/G,
+         R = R/G) %>%
+  select(lgID, HR, BB, R) 
+
+# a TRUE 
+dat %>%
+  group_by(lgID) %>%
+  do(tidy(lm(R ~ HR, data= .),conf.int = T)) %>%
+  filter( term == "HR")
+  
+# b
+dat %>% 
+  group_by(lgID) %>% 
+  do(glance(lm(R ~ HR, data = .)))
+
+# c
+dat %>% 
+  do(tidy(lm(R ~ HR, data = .), conf.int = T)) %>% 
+  filter(term == "HR")
+
+# d
+dat %>% 
+  group_by(lgID) %>% 
+  do(mod = lm(R ~ HR, data = .))
+
+
+# q8
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+# set.seed(1) # if you are using R 3.5 or earlier
+set.seed(1, sample.kind = "Rounding") # if you are using R 3.6 or later
+galton <- GaltonFamilies %>%
+  group_by(family, gender) %>%
+  sample_n(1) %>%
+  ungroup() %>% 
+  gather(parent, parentHeight, father:mother) %>%
+  mutate(child = ifelse(gender == "female", "daughter", "son")) %>%
+  unite(pair, c("parent", "child"))
+
+head(galton, 10)
+
+galton %>%
+  group_by(pair)  %>%
+  summarize( n= n())
+
+# q9 find the stongest corelation
+galton %>%
+  group_by(pair)  %>%
+  summarize(cor = cor(parentHeight,childHeight)) %>%
+  filter(cor == max(cor))
+  
+galton %>%
+  group_by(pair)  %>%
+  summarize(cor = cor(parentHeight,childHeight)) %>%
+  filter(cor == min(cor))
+  # do(tidy(lm(childHeight ~ parentHeight, data= .),conf.int = T)) %>%
+  # filter( term == "parentHeight")
+
+# q10
+library(broom)
+
+galton %>%
+group_by(pair) %>%
+  do(tidy(lm(childHeight ~ parentHeight, data = .), conf.int = TRUE)) %>%
+  filter(term == "parentHeight")
+  
+galton %>%
+  group_by(pair) %>%
+  do(tidy(lm(childHeight ~ parentHeight, data = .), conf.int = TRUE)) %>%
+   filter(term == "parentHeight") %>%
+  # filter(term == "parentHeight" & p.value < .05)
+  select(pair, estimate, conf.low, conf.high) %>%
+  ggplot(aes(pair, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  geom_errorbar() +
+  geom_point()
+
+galton %>%
+  group_by(pair) %>%
+  do(tidy(lm(childHeight ~ parentHeight, data = .), conf.int = TRUE)) %>%
+  filter(term == "parentHeight") %>%
+  # filter(term == "parentHeight" & p.value < .05)
+  select(pair, estimate, conf.low, conf.high) %>%
+  ggplot(aes(pair, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  geom_errorbar() +
+  geom_point()
+
 
 
 
