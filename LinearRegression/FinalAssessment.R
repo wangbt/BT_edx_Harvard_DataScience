2d9d8aee35bff58043ae23cd2e586782ad5ce3a9
wangbt <wangbertone@hotmail.com>
2021-05-18 00:41:22 -0500

    final assess

---
+++ LinearRegression/FinalAssessment.R
@@ -0,0 +1,11 @@
+# Final assessment #####
+library(dslabs)
+data("research_funding_rates")
+?research_funding_rates
+
+# Q1
+
+
+
+
+
--- LinearRegression/Sec3.R
+++ LinearRegression/Sec3.R
@@ -1,5 +1,8 @@
 # Sec 3 Confounding ####
 library(broom)
+library(HistData)
+library(tidyverse)
+library(tidyr)
 
 # generate the Monte Carlo simulation
 N <- 25
@@ -78,6 +81,7 @@ admissions %>% group_by(gender) %>%
   summarize(percentage = 
               round(sum(admitted*applicants)/sum(applicants),1))
 
+
 # test whether gender and admission are independent
 admissions %>% group_by(gender) %>% 
   summarize(total_admitted = round(sum(admitted / 100 * applicants)), 
@@ -114,7 +118,8 @@ admissions %>%
   geom_bar(stat = "identity", position = "stack")
 
 # condition on major and then look at differences
-admissions %>% ggplot(aes(major, admitted, col = gender, size = applicants)) + geom_point()
+admissions %>% 
+  ggplot(aes(major, admitted, col = gender, size = applicants)) + geom_point()
 
 # average difference by major
 admissions %>%  group_by(gender) %>% summarize(average = mean(admitted))
